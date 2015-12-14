e2e27d6dacc2650dd8e2cb2f4538da92697597fc
Heiko Beineke <hbeineke@fuchs.de>
2015-12-14 19:28:45 +0100

    Finalize first introduction layout

--- app/assets/javascripts/applicant.js
+++ app/assets/javascripts/applicant.js
@@ -1,4 +1,8 @@
 $(function() {
+  // init introduction
+  $('#introduction .images .card:eq(0)').addClass('active');
+  $('#introduction .descriptions .collapse:eq(0)').addClass('in');
+
   $('#introduction').find('.card').mouseenter(function() {
     $(this).find('img').animate({opacity: 1}, 500);
   });
@@ -9,7 +13,13 @@ $(function() {
   });
 
   $('#introduction').find('.card').click(function() {
+    // image animation
     $(this).parent().find('.card').find('img').animate({opacity: 0.4}, 500).parent().removeClass('active');
     $(this).animate({opacity: 1}, 500).delay(500).addClass('active');
+
+    // collapse handling
+    id = $(this).attr('href');
+    $('.descriptions').find('.collapse').collapse('hide');
+    $(id).collapse('show');
   });
 });
