7fc2f22f54bce0ff0f83de957011d424148889d0
Heiko Beineke <hbeineke@fuchs.de>
2015-12-28 13:21:40 +0100

    Refactor introduction

--- app/assets/javascripts/applicant.js
+++ app/assets/javascripts/applicant.js
@@ -1,29 +1,38 @@
 $(function() {
   // init introduction
-  $('#introduction .images .card:eq(0)').addClass('active');
-  $('#introduction .descriptions .collapse:eq(0)').addClass('in');
+  $('#introduction .card:eq(0)').addClass('active');
 
   $('#introduction').find('.card').mouseenter(function() {
-    $(this).find('img').animate({opacity: 1}, 500);
+    card_in($(this))
   });
 
   $('#introduction').find('.card').mouseout(function() {
-    if(!$(this).hasClass('active'))
-      $(this).find('img').animate({opacity: 0.4}, 500);
+    if(!$(this).hasClass('active')){
+      card_out($(this))
+    }
   });
 
   $('#introduction').find('.card').click(function() {
-    // image animation
-    $(this).parent().find('.card').find('img').animate({opacity: 0.4}, 500).parent().removeClass('active');
-    $(this).animate({opacity: 1}, 500).delay(500).addClass('active');
+    cards = $(this).parent().find('.card')
+    card_out(cards);
+    cards.removeClass('active');
 
-    // collapse handling
-    id = $(this).attr('href');
-    $('.descriptions').find('.collapse').collapse('hide');
-    $(id).collapse('show');
+    $(this).addClass('active');
   });
 
   $('.toggle_collapse_icon').click(function() {
     $(this).toggleClass('fa-chevron-circle-up fa-chevron-circle-down')
   });
 });
+
+function card_in(selector) {
+  selector.find('img').animate({opacity: 1}, 500);
+  selector.find('.card-title').animate({opacity: 1}, 500);
+  selector.find('.card-text').animate({opacity: 1}, 500);
+}
+
+function card_out(selector) {
+  selector.find('img').animate({opacity: 0.4}, 500);
+  selector.find('.card-title').animate({opacity: 0}, 500);
+  selector.find('.card-text').animate({opacity: 0}, 500);
+}
