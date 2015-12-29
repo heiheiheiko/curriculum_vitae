8c2cde8d3b6bcdb83ac50efc18cfac31b608b169
Heiko Beineke <hbeineke@fuchs.de>
2015-12-29 10:09:38 +0100

    Remove hover effect

--- app/assets/javascripts/applicant.js
+++ app/assets/javascripts/applicant.js
@@ -2,19 +2,11 @@ $(function() {
   // introduction
   $('#introduction .card:eq(0)').addClass('active');
 
-  $('#introduction').find('.card').mouseenter(function() {
-    card_in($(this))
-  });
-
-  $('#introduction').find('.card').mouseout(function() {
-    if(!$(this).hasClass('active')){
-      card_out($(this))
-    }
-  });
-
   $('#introduction').find('.card').click(function() {
     cards = $(this).parent().find('.card')
-    card_out(cards);
+    cards.find('img').animate({opacity: 0.4}, 500);
+    cards.find('.card-title').animate({opacity: 0}, 500);
+    cards.find('.card-text').animate({opacity: 0}, 500);
     cards.removeClass('active');
 
     $(this).addClass('active');
@@ -53,15 +45,3 @@ $(function() {
     $(this).toggleClass('fa-chevron-circle-up fa-chevron-circle-down')
   });
 });
-
-function card_in(selector) {
-  selector.find('img').animate({opacity: 1}, 500);
-  selector.find('.card-title').animate({opacity: 1}, 500);
-  selector.find('.card-text').animate({opacity: 1}, 500);
-}
-
-function card_out(selector) {
-  selector.find('img').animate({opacity: 0.4}, 500);
-  selector.find('.card-title').animate({opacity: 0}, 500);
-  selector.find('.card-text').animate({opacity: 0}, 500);
-}
