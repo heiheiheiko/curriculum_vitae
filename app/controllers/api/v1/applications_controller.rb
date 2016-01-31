4700159b3fd502e8f35386ec3fa006499c6107a0
Heiko Beineke <hbeineke@fuchs.de>
2016-01-31 03:15:24 +0100

    Access application api by auth_token

--- app/controllers/api/v1/applications_controller.rb
+++ app/controllers/api/v1/applications_controller.rb
@@ -1,17 +1,8 @@
 module Api
   module V1
     class ApplicationsController < ApplicationController
-      before_action :authenticate
-
       def show
-        @application = Application.find(params[:id])
-      end
-
-      protected
-      def authenticate
-        unless Application.find_by(auth_token: params[:auth_token])
-          raise ActionController::RoutingError.new('Not Found')
-        end
+        @application = Application.find_by(auth_token: params[:id])
       end
     end
   end
