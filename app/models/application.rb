85650b21f030814754bc52999fbba67043690c87
Heiko Beineke <hbeineke@fuchs.de>
2016-01-31 03:59:06 +0100

    Complete application api ... again

--- app/models/application.rb
+++ app/models/application.rb
@@ -1,7 +1,9 @@
 class Application < ActiveRecord::Base
+  before_create :generate_auth_token
+
   belongs_to :applicant
   has_one :address, as: :addressable, dependent: :destroy
-  before_create :generate_auth_token
+  has_one :job_advertisement, as: :linkable, class_name: 'Website', dependent: :destroy
 
   private
   def generate_auth_token
