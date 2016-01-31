86df673162276d63dc99aa9c52a9b852d3e5dca0
Heiko Beineke <hbeineke@fuchs.de>
2016-01-31 03:47:34 +0100

    Handle not found request

--- app/controllers/api/v1/applications_controller.rb
+++ app/controllers/api/v1/applications_controller.rb
@@ -2,7 +2,7 @@ module Api
   module V1
     class ApplicationsController < ApplicationController
       def show
-        @application = Application.find_by(auth_token: params[:id])
+        @application = Application.find_by(auth_token: params[:id]) || not_found
       end
     end
   end
