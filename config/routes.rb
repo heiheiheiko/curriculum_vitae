d10209e7432b95998d340c5c510e3804c4a2df8d
Heiko Beineke <heiko.beineke@gmail.com>
2018-12-17 18:41:32 +0100

    Setup i18n by routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   root to: 'applicants#show'
+  get '/:locale' => 'applicants#show'
+
+  scope "(:locale)", locale: /en|de/ do
+    resources :applicants, only: [:show]
+  end
 
   namespace :api do
     namespace :v1 do
