5b65c90a73f8a76d7076dcb1730c485ce30b83f7
Heiko Beineke <heiko.beineke@gmail.com>
2018-12-18 03:16:23 +0100

    Sort skills

--- app/lib/skill_overview.rb
+++ app/lib/skill_overview.rb
@@ -4,9 +4,15 @@ class SkillOverview
   end
 
   def skill_type_groups
-    @skill_type_groups ||=
-      @items.
+    skill_type_groups = @items.
         group_by { |cis| cis.skill.information_type }.
         map { |k, v| SkillOverviews::SkillTypeGroup.new(k, v) }
+    
+    @skill_type_groups ||= [
+      skill_type_groups.find { |stg| stg.type == 'software' },
+      skill_type_groups.find { |stg| stg.type == 'technology' },
+      skill_type_groups.find { |stg| stg.type == 'area' },
+      skill_type_groups.find { |stg| stg.type == 'conditions' }
+    ]
   end
 end
