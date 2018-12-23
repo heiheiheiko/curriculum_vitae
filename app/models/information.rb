727cb410a4e98dbb406cb003223d49b230e81395
Heiko Beineke <heiko.beineke@gmail.com>
2018-12-23 16:57:17 +0100

    Move extend_decorators to information

--- app/models/information.rb
+++ app/models/information.rb
@@ -2,4 +2,13 @@ class Information < ApplicationRecord
   belongs_to :informable, polymorphic: true
 
   translates :title, :description
+
+  def extend_decorators
+    case information_type
+    when "personal"
+      extend Personal
+    when "professional"
+      extend Professional
+    end
+  end
 end
--- app/models/interest.rb
+++ app/models/interest.rb
@@ -1,12 +1,3 @@
 class Interest < Information
   enum information_type: [ :personal, :professional ]
-
-  def extend_decorators
-    case information_type
-    when "personal"
-      extend Personal
-    when "professional"
-      extend Professional
-    end
-  end
 end
