33ca6ce57abb0375ec9df88e7dbade0d8e7c79d4
Heiko Beineke <heiko.beineke@gmail.com>
2019-01-02 00:18:51 +0100

    Add eslint

---
+++ .eslintrc.js
@@ -0,0 +1,33 @@
+const config = {
+  env: {
+      browser: true,
+      es6: true,
+      node: true
+  },
+  parserOptions: {
+      parser: 'babel-eslint',
+      sourceType: 'module',
+      allowImportExportEverywhere: true,
+  },
+  extends: [
+      'airbnb-base',
+  ],
+  plugins: [],
+  rules: {
+    'indent': ['error', 2],
+    'no-param-reassign': ['error', {
+        ignorePropertyModificationsFor: [
+            'el',
+        ],
+    }],
+    'no-shadow': ['error', {
+        allow: [
+            'state',
+        ],
+    }],
+    'quotes': ['error', 'single'],
+    'import/prefer-default-export': 'off',
+  }
+};
+
+module.exports = config;
