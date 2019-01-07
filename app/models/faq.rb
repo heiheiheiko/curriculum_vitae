91ab1f3f02a7b977ec5d8d82b93a03ec67138f5a
Heiko Beineke <heiko.beineke@gmail.com>
2019-01-07 10:32:15 +0100

    Code cosmetic :lipstick:

--- app/models/faq.rb
+++ app/models/faq.rb
@@ -10,4 +10,11 @@ class Faq < Information
       extend Professional
     end
   end
+
+  # decorator
+  def avatar_asset
+    personal_image = "explorer_40x40.jpg"
+    professional_image = "accomplisher_40x40.jpg"
+    information_type == "personal" ? personal_image : professional_image
+  end
 end
